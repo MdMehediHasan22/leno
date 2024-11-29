35150f40f84c357f607ed5603613f1dcf5580baf
MdMehediHasan22 <mdmehedi3799@gmail.com>
2024-11-29 17:50:22 +0600

    cmplt the video button

--- js/script.js
+++ js/script.js
@@ -4,8 +4,37 @@ document.addEventListener('DOMContentLoaded',function(){
 
     toggleButton.addEventListener('click',function(){
         mobileMenu.classList.toggle('active');
-    })
-})
+    });
+     // Video Modal
+  const modal = document.getElementById('videoModal');
+  const videoButton = document.querySelector('.preview_video-button');
+  const closeButton = document.querySelector('.modal_close-button');
+  const videoPlayer = document.getElementById('videoPlayer');
+
+  // Open modal when clicked
+  videoButton.addEventListener('click', function () {
+    // Show modal
+    modal.style.display = 'block';
+
+    //Replace the src attribute with the video URL
+    videoPlayer.src = 'https://www.youtube.com/embed/8sXRyHI3bLw';
+
+    // Close modal on close button click
+    closeButton.addEventListener('click', function () {
+      modal.style.display = 'none';
+      videoPlayer.src = '';
+    });
+
+    // Close modal on outter click
+    window.addEventListener('click', function (event) {
+      if (event.target == modal) {
+        modal.style.display = 'none';
+        videoPlayer.src = '';
+      }
+    });
+  });
+
+});
 
 // scrolling
 window.addEventListener('scroll',function(){
